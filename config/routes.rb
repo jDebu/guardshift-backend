cda3b9c3e1d9f46cc0ef64b5ca4847c005b26ad8
manuel delgado <mdelgado.pcm@gmail.com>
2024-08-07 05:30:56 -0500

    feat: add scope to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :availabilities, only: [:index, :create]
-  resources :shifts, only: [:index] do
-    collection do
-      get 'weeks', to: 'shifts#weeks'
+  scope '/backend' do
+    resources :availabilities, only: [:index, :create]
+    resources :shifts, only: [:index] do
+      collection do
+        get 'weeks', to: 'shifts#weeks'
+      end
     end
+    resources :services, only: [:index]
   end
-  resources :services, only: [:index]
 end
